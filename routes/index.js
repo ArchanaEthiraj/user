43ad6732324074d1556b71cc1195321c3bd7e96c
ArchanaEthiraj <archana@doodle-blue.com>
2024-11-17 23:18:06 +0530

    Booking Removed

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const initializeRoutes = (app) => {
     app.use('/api/v1/user', require('./v1/userRoutes.js'));
     app.use('/api/v1/shop', require('./v1/shopRoutes.js'));
-    app.use('/api/v1/booking', require('./v1/bookingRoutes.js'));
 };
 
 module.exports = initializeRoutes;
